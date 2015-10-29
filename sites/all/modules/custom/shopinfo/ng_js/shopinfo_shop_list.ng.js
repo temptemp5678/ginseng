8d786ce5f5b67bf0793c9091f6782f208b663050
DongFlexxia <dong@flexxia.ca>
2015-10-28 23:53:13 -0400

    read Json file

--- sites/all/modules/custom/shopinfo/ng_js/shopinfo_shop_list.ng.js
+++ sites/all/modules/custom/shopinfo/ng_js/shopinfo_shop_list.ng.js
@@ -9,12 +9,13 @@
    */
   var app = angular.module('shopList', []);
   app.controller('shopListController', ['$scope', '$http', function($scope, $http) {
-    var jsonFilePath = basePathUrl + 'sites/default/files/all_ginseng.json';
     var jsonFilePath = basePathUrl + 'sites/default/files/json/source/all_ginseng.json';
+    // var jsonFilePath = basePathUrl + 'system/files/json/source/all_ginseng.json';
 
     $http.get(jsonFilePath)
       .success(function (data) {
          $scope.sourcedata = data.nodes.ginseng;
+console.log($scope.sourcedata);
       })
       .error(function (data, status, headers, config) {
          //  Do some error handling here
