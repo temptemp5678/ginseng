141f4ebb1a5d4861cc74216e6d6e9875f4f04372
DongFlexxia <dong@flexxia.ca>
2015-12-13 10:28:10 -0500

    clean code

--- sites/all/modules/custom/shopinfo/ng_js/shopinfo_shop_list.ng.js
+++ sites/all/modules/custom/shopinfo/ng_js/shopinfo_shop_list.ng.js
@@ -1,26 +1,38 @@
 /**
  * file
  */
-(function ($) {
-  var basePathUrl = Drupal.settings.basePath;
+var basePathUrl = Drupal.settings.basePath;
 
-  /**
-   * AngularJS
-   */
-  var app = angular.module('shopList', []);
-  app.controller('shopListController', ['$scope', '$http', function($scope, $http) {
-    var jsonFilePath = basePathUrl + 'sites/default/files/json/source/all_ginseng.json';
-    // var jsonFilePath = basePathUrl + 'system/files/json/source/all_ginseng.json';
+/**
+ * AngularJS
+ */
+var app = angular.module('shopList', []);
 
-    $http.get(jsonFilePath)
-      .success(function (data) {
-         $scope.sourcedata = data.nodes.ginseng;
-console.log($scope.sourcedata);
-      })
-      .error(function (data, status, headers, config) {
-         //  Do some error handling here
-      });
+// 使用factory severice取得JSON文件
+app.factory("mainData", ['$http', function($http) {
+  // var jsonFilePath = basePathUrl + 'sites/default/files/json/source/all_ginseng.json';
+  var jsonFilePath = basePathUrl + 'api/json/node/all-ginseng-data';
+  var obj = {};
 
-  }]);
+  // 自建一个函数，来返回$http.get取得的文件内容
+  obj.fetchUserDetails = function(){
+    return $http.get(jsonFilePath);
 
-})(jQuery);
+    // 使用factory也可以返回post的值
+    $http.post(pageUrl, json)
+  }
+
+  return obj;
+}]);
+
+app.controller('shopListController', ['$scope', '$http', 'mainData', function($scope, $http, mainData) {
+  mainData.fetchUserDetails().success(function(data) {
+    // 通过mainData传递的值，来设置$scope.userProfile的初始值
+    $scope.sourcedata = data.nodes.ginseng;
+  })
+  .error(function (data, status, headers, config) {
+     //  Do some error handling here
+  });
+
+}]);
+
